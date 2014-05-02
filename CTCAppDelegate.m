ba1b3cba1fd9e94bc4a48d6b048cd125bf22e0ae
Giorgio Calderolla <giorgio.calderolla@icloud.com>
2014-05-02 18:45:30 +0200

    Move constant strings to defaults

--- CTCAppDelegate.m
+++ CTCAppDelegate.m
@@ -3,16 +3,6 @@
 #import "Preferences.h"
 
 
-// Constant, non-localized, non-UI-related strings
-NSString * const APPLICATION_WEBSITE_URL = @"http://github.com/mipstian/catch";
-NSString * const APPLICATION_BUG_REPORT_URL = @"https://github.com/mipstian/catch/issues/new";
-NSString * const APPLICATION_FEATURE_REQUEST_URL = @"https://github.com/mipstian/catch/issues/new";
-NSString * const APPLICATION_HELP_URL = @"https://github.com/mipstian/catch/wiki/Configuration";
-NSString * const SERVICE_URL = @"http://showrss.info/";
-NSString * const SERVICE_FEED_URL_PREFIX = @"http://showrss.info/rss.php?";
-NSString * const SERVICE_FEED_LEGACY_URL_PREFIX = @"http://showrss.karmorra.info/rss.php?";
-
-
 @interface CTCAppDelegate ()
 @property (strong, nonatomic) IBOutlet CTCMainController *mainController;
 @end
--- CTCDefaults.h
+++ CTCDefaults.h
@@ -3,6 +3,14 @@
 
 extern NSTimeInterval const kCTCDefaultsFeedUpdateInterval;
 
+extern NSString * const APPLICATION_WEBSITE_URL;
+extern NSString * const APPLICATION_BUG_REPORT_URL;
+extern NSString * const APPLICATION_FEATURE_REQUEST_URL;
+extern NSString * const APPLICATION_HELP_URL;
+extern NSString * const SERVICE_URL;
+extern NSString * const SERVICE_FEED_URL_PREFIX;
+extern NSString * const SERVICE_FEED_LEGACY_URL_PREFIX;
+
 
 @interface CTCDefaults : NSObject
 
