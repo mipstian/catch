6c8a3fd5fd7a7ca47fb460ba3aa516dae4acbac4
Giorgio Calderolla <giorgio.calderolla@icloud.com>
2014-05-02 19:08:30 +0200

    Move more stuff out of Preferences

--- Preferences.m
+++ Preferences.m
@@ -1,5 +1,4 @@
 #import "Preferences.h"
-#import "CTCFileUtils.h"
 #import "CTCDefaults.h"
 
 
@@ -18,83 +17,6 @@ NSString * const PREFERENCE_KEY_OPEN_AT_LOGIN = @"openAtLogin";
 
 @implementation Preferences
 
-+ (void)setDefaultDefaults {
-	// Create two dummy times (dates actually), just to have some value set
-	NSDateComponents *comps = NSDateComponents.new;
-    comps.hour = 24;
-    comps.minute = 0;
-	NSCalendar *calendar = NSCalendar.currentCalendar;
-	NSDate *dateFrom = [calendar dateFromComponents:comps];
-    comps.hour = 8;
-	NSDate *dateTo = [calendar dateFromComponents:comps];
-	
-	// Search for user's Downloads directory (by the book)
-	NSString *downloadsDirectory;
-	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDownloadsDirectory, NSUserDomainMask, YES);
-	if (paths.count > 0) {
-		downloadsDirectory = paths.firstObject;
-		NSLog(@"Default save path is %@", downloadsDirectory);
-	} else {
-		// Default to ~/Downloads/ (at this point it probably won't work though)
-		NSLog(@"Defaulting to ~/Downloads/ for save path");
-		downloadsDirectory = @"~/Downloads/";
-	}
-	
-	// Set smart defaults for the preferences
-	NSDictionary *appDefaults = @{PREFERENCE_KEY_FEED_URL: @"",
-                                  PREFERENCE_KEY_ONLY_UPDATE_BETWEEN: @NO,
-                                  PREFERENCE_KEY_UPDATE_FROM: dateFrom,
-                                  PREFERENCE_KEY_UPDATE_TO: dateTo,
-                                  PREFERENCE_KEY_SAVE_PATH: downloadsDirectory,
-                                  PREFERENCE_KEY_ORGANIZE_TORRENTS: @NO,
-                                  PREFERENCE_KEY_OPEN_AUTOMATICALLY: @YES,
-                                  PREFERENCE_KEY_SEND_NOTIFICATIONS: @YES,
-                                  PREFERENCE_KEY_OPEN_AT_LOGIN: @YES};
-	[NSUserDefaults.standardUserDefaults registerDefaults:appDefaults];
-    
-	// Migrate the downloads history format. Change old array of strings to new dictionary format
-	NSArray *downloadedFiles = [NSUserDefaults.standardUserDefaults arrayForKey:PREFERENCE_KEY_DOWNLOADED_FILES];
-	NSArray *history = [NSUserDefaults.standardUserDefaults arrayForKey:PREFERENCE_KEY_HISTORY];
-	if (downloadedFiles && !history) {
-		NSLog(@"Migrating download history to new format.");
-		
-		NSMutableArray *newDownloadedFiles = NSMutableArray.array;
-		
-		for (NSString *url in downloadedFiles) {
-			[newDownloadedFiles addObject:[NSDictionary dictionaryWithObjectsAndKeys:
-                                           [CTCFileUtils computeFilenameFromURL:[NSURL URLWithString:url]], @"title",
-                                           url, @"url",
-                                           nil]];
-		}
-		
-		[NSUserDefaults.standardUserDefaults setObject:newDownloadedFiles
-                                                forKey:PREFERENCE_KEY_HISTORY];
-		[NSUserDefaults.standardUserDefaults removeObjectForKey:PREFERENCE_KEY_DOWNLOADED_FILES];
-	}
-    
-    // If history was never set or migrated, init it to empty array
-    if (!downloadedFiles && !history) {
-        [NSUserDefaults.standardUserDefaults setObject:@[]
-                                                forKey:PREFERENCE_KEY_HISTORY];
-    }
-    
-    // Register as a login item if needed
-    [CTCDefaults refreshLoginItemStatus];
-}
-
-+ (NSString *)feedURL {
-	NSString *rawFeedURL = [NSUserDefaults.standardUserDefaults stringForKey:PREFERENCE_KEY_FEED_URL];
-	return [rawFeedURL stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet];
-}
-
-+ (void)save {
-	// Write preferences to disk
-	[NSUserDefaults.standardUserDefaults synchronize];
-    
-    // Register as a login item if needed
-    [CTCDefaults refreshLoginItemStatus];
-}
-
 + (BOOL)isConfigurationValid {
 	// Validate torrent folder. This should never fail!
 	NSString *torrentFolder = [NSUserDefaults.standardUserDefaults stringForKey:PREFERENCE_KEY_SAVE_PATH];
@@ -111,7 +33,7 @@ NSString * const PREFERENCE_KEY_OPEN_AT_LOGIN = @"openAtLogin";
 	}
 
 	// Most importantly, validate feed URL
-	NSString *feedURL = self.feedURL;
+	NSString *feedURL = CTCDefaults.feedURL;
 	NSRange range = [feedURL rangeOfString:kCTCDefaultsServiceFeedURLPrefix];
 	if (range.location != 0) {
 		// Try the legacy URL prefix and consider that valid
