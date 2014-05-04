e0a881fe04dd385d869bd05b9eb3fab9a0123cc0
Giorgio Calderolla <giorgio.calderolla@icloud.com>
2014-05-05 00:10:31 +0200

    Dedicated “browser” class

---
+++ CTCBrowser.m
@@ -0,0 +1,32 @@
+#import "CTCBrowser.h"
+#import "CTCDefaults.h"
+
+
+@implementation CTCBrowser
+
+- (IBAction)browseService:(id)sender {
+	// Launch the system browser, open the service (ShowRSS)
+	[NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:kCTCDefaultsServiceURL]];
+}
+
+- (IBAction)browseWebsite:(id)sender {
+	// Launch the system browser, open the applications's website
+	[NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:kCTCDefaultsApplicationWebsiteURL]];
+}
+
+- (IBAction)browseHelp:(id)sender {
+	// Launch the system browser, open the applications's on-line help
+	[NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:kCTCDefaultsApplicationHelpURL]];
+}
+
+- (IBAction)browseFeatureRequest:(id)sender {
+	// Launch the system browser, open the applications's feature request page
+	[NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:kCTCDefaultsApplicationFeatureRequestURL]];
+}
+
+- (IBAction)browseBugReport:(id)sender {
+	// Launch the system browser, open the applications's bug report page
+	[NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:kCTCDefaultsApplicationBugReportURL]];
+}
+
+@end
