2a5e064c91f24e66479022a61aa6f409d9d43829
Giorgio Calderolla <giorgio.calderolla@icloud.com>
2014-05-04 15:41:16 +0200

    Finished fixing project, fix new warnings

--- CTCApplication.m
+++ CTCApplication.m
@@ -11,30 +11,24 @@
     if (event.type == NSKeyDown) {
         if ((event.modifierFlags & NSDeviceIndependentModifierFlagsMask) == NSCommandKeyMask) {
             if ([event.charactersIgnoringModifiers isEqualToString:@"x"]) {
-                if ([self sendAction:@selector(cut:) to:nil from:self])
-                    return;
+                if ([self sendAction:@selector(cut:) to:nil from:self]) return;
             }
             else if ([event.charactersIgnoringModifiers isEqualToString:@"c"]) {
-                if ([self sendAction:@selector(copy:) to:nil from:self])
-                    return;
+                if ([self sendAction:@selector(copy:) to:nil from:self]) return;
             }
             else if ([event.charactersIgnoringModifiers isEqualToString:@"v"]) {
-                if ([self sendAction:@selector(paste:) to:nil from:self])
-                    return;
+                if ([self sendAction:@selector(paste:) to:nil from:self]) return;
             }
             else if ([event.charactersIgnoringModifiers isEqualToString:@"z"]) {
-                if ([self sendAction:@selector(undo:) to:nil from:self])
-                    return;
+                if ([self sendAction:NSSelectorFromString(@"undo:") to:nil from:self]) return;
             }
             else if ([event.charactersIgnoringModifiers isEqualToString:@"a"]) {
-                if ([self sendAction:@selector(selectAll:) to:nil from:self])
-                    return;
+                if ([self sendAction:@selector(selectAll:) to:nil from:self]) return;
             }
         }
         else if ((event.modifierFlags & NSDeviceIndependentModifierFlagsMask) == (NSCommandKeyMask | NSShiftKeyMask)) {
             if ([event.charactersIgnoringModifiers isEqualToString:@"Z"]) {
-                if ([self sendAction:@selector(redo:) to:nil from:self])
-                    return;
+                if ([self sendAction:NSSelectorFromString(@"redo:") to:nil from:self]) return;
             }
         }
     }
