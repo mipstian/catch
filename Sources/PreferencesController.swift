5b0eb99fac23b517135b20ca788c5429b2ef9268
Giorgio Calderolla <giorgio.calderolla@icloud.com>
2016-10-22 23:24:34 +0200

    Swiftify preferences controller

---
+++ Sources/PreferencesController.swift
@@ -0,0 +1,78 @@
+import AppKit
+
+
+class PreferencesController: NSWindowController {
+  @IBOutlet private weak var feedURLWarningImageView: NSImageView!
+  @IBOutlet private weak var torrentsSavePathWarningImageView: NSImageView!
+  
+  override func awakeFromNib() {
+    showFeeds(self)
+    
+    // If the configuration isn't valid, pop up immediately
+    if !CTCDefaults.isConfigurationValid() { showWindow(self) }
+    
+    UserDefaults.standard.addObserver(self, forKeyPath: "savePath", options: .new, context: nil)
+    UserDefaults.standard.addObserver(self, forKeyPath: "feedURL", options: .new, context: nil)
+  }
+  
+  override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey:Any]?, context: UnsafeMutableRawPointer?) {
+    refreshInvalidInputMarkers()
+  }
+  
+  fileprivate func refreshInvalidInputMarkers() {
+    torrentsSavePathWarningImageView.image = CTCDefaults.isTorrentsSavePathValid() ? #imageLiteral(resourceName: "success") : #imageLiteral(resourceName: "warning")
+    feedURLWarningImageView.image = CTCDefaults.isFeedURLValid() ? #imageLiteral(resourceName: "success") : #imageLiteral(resourceName: "warning")
+  }
+  
+  deinit {
+    UserDefaults.standard.removeObserver(self, forKeyPath: "savePath")
+    UserDefaults.standard.removeObserver(self, forKeyPath: "feedURL")
+  }
+}
+
+
+// MARK: Actions
+extension PreferencesController {
+  @IBAction override func showWindow(_ sender: Any?) {
+    refreshInvalidInputMarkers()
+    NSApp.activate(ignoringOtherApps: true)
+    super.showWindow(sender)
+  }
+  
+  @IBAction private func savePreferences(_: Any?) {
+    CTCDefaults.save()
+    
+    if CTCDefaults.isConfigurationValid() {
+      // Hide the Preferences window
+      window?.close()
+      
+      // Apply the login item setting
+      // TODO: move to defaults
+      CTCDefaults.refreshLoginItemStatus()
+      
+      // Apply power management settings
+      // TODO: move to scheduler
+      CTCScheduler.shared().refreshActivity()
+      
+      // Also force check
+      CTCScheduler.shared().forceCheck()
+    }
+    else {
+      // Show the Feeds tab because all possible invalid inputs are currently there
+      showFeeds(self)
+      
+      // Shake the window to signal invalid input
+      window?.performShakeAnimation()
+    }
+  }
+  
+  @IBAction fileprivate func showFeeds(_: Any?) {
+    // Select the Feeds tab
+    window?.toolbar?.selectedItemIdentifier = "Feed"
+  }
+  
+  @IBAction private func showTweaks(_: Any?) {
+    // Select the Tweaks tab
+    window?.toolbar?.selectedItemIdentifier = "Tweaks"
+  }
+}
