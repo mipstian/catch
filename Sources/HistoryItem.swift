6fda52653fd99574ca2dc148a0d2117e0fcf3d2d
Giorgio Calderolla <giorgio.calderolla@icloud.com>
2016-11-05 22:09:47 +0100

    Promote history dictionaries to structs

---
+++ Sources/HistoryItem.swift
@@ -0,0 +1,21 @@
+struct HistoryItem {
+  let title: String
+  let url: URL
+  let downloadDate: Date? // Very old items might not have a date set
+  let isMagnetLink: Bool
+}
+
+
+extension HistoryItem {
+  var dictionaryRepresentation: [AnyHashable:Any] {
+    var dictionary: [AnyHashable:Any] = [
+      "title": title,
+      "url": url.absoluteString,
+      "isMagnetLink": isMagnetLink
+    ]
+    if let downloadDate = downloadDate {
+      dictionary["date"] = downloadDate
+    }
+    return dictionary
+  }
+}
