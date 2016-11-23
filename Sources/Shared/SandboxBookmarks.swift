1a7e561ab0a941d589cd51c379a2f240c8fa08d9
Giorgio Calderolla <giorgio.calderolla@icloud.com>
2016-11-23 01:31:04 +0100

    Factor out (and explain) sandbox bookmark code

---
+++ Sources/Shared/SandboxBookmarks.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+
+// In a sandboxed environment, URL bookmarks can be passed from an app to an unpriviledged
+// XPC service in order to allow it access to a filesystem location. Catch uses this mechanism
+// to transfer the main app's access to the download directory (granted by the user via powerbox)
+// to the feed helper service.
+
+
+extension Data {
+  static func sandboxBookmark(for url: URL) throws -> Data {
+    return try url.bookmarkData(options: .minimalBookmark)
+  }
+}
+
+
+extension URL {
+  init(sandboxBookmark: Data) throws {
+    var isStale = false
+    
+    do {
+      // TODO: figure out why this init is optional, which contradicts the docs
+      self = try URL(resolvingBookmarkData: sandboxBookmark, bookmarkDataIsStale: &isStale)!
+    } catch {
+      NSLog("Could not get URL from bookmark: \(error)")
+      throw error
+    }
+  }
+}
