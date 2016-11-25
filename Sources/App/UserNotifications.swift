6df769b6381a59429a87d5c913b652fdb3170be9
Giorgio Calderolla <giorgio.calderolla@icloud.com>
2016-11-25 15:03:42 +0100

    Gardening

--- Sources/App/UserNotifications.swift
+++ Sources/App/UserNotifications.swift
@@ -2,12 +2,12 @@ import Foundation
 
 
 extension NSUserNotificationCenter {
-  func deliverNotification(newEpisode: Episode) {
+  func deliverNewEpisodeNotification(for episode: Episode) {
     let notification = NSUserNotification()
     notification.title = NSLocalizedString("newtorrent", comment: "New torrent notification")
     notification.informativeText = .localizedStringWithFormat(
       NSLocalizedString("newtorrentdesc", comment: "New torrent notification"),
-      newEpisode.title
+      episode.title
     )
     notification.soundName = NSUserNotificationDefaultSoundName
     deliver(notification)
