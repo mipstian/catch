3b54783a41be456e2012da8ff6ac16b29def5509
Giorgio Calderolla <giorgio.calderolla@icloud.com>
2020-03-29 10:08:43 +0200

    Refactor

---
+++ Sources/App/WorkspaceExtensions.swift
@@ -0,0 +1,20 @@
+import AppKit
+
+
+extension NSWorkspace {
+  func openInBackground(url: URL) {
+    // Open a link without bringing app that handles it to the foreground
+    open(
+      [url],
+      withAppBundleIdentifier: nil,
+      options: .withoutActivation,
+      additionalEventParamDescriptor: nil,
+      launchIdentifiers: nil
+    )
+  }
+  
+  func openInBackground(file: String) {
+    // Open a file without bringing app that handles it to the foreground
+    openFile(file, withApplication: nil, andDeactivate: false)
+  }
+}
