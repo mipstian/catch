20f8d376b524428f0f6fdcac0e64412d95a14b63
Giorgio Calderolla <giorgio.calderolla@icloud.com>
2020-05-06 11:18:05 +0200

    Only add script-downloaded episodes to history if script succeeds

--- Sources/App/Scripts.swift
+++ Sources/App/Scripts.swift
@@ -2,12 +2,22 @@ import Foundation
 
 
 extension Process {
-  static func runDownloadScript(url: URL) {
+  static func runDownloadScript(url: URL, completion: ((Bool) -> ())? = nil) {
     if Defaults.shared.downloadScriptEnabled, let downloadScriptPath = Defaults.shared.downloadScriptPath {
-      Self.launchedProcess(
-        launchPath: downloadScriptPath.path,
-        arguments: [url.absoluteString]
-      )
+      let script = Process()
+      script.launchPath = downloadScriptPath.path
+      script.arguments = [url.absoluteString]
+      script.terminationHandler = { process in
+        DispatchQueue.main.async {
+          let success = process.terminationStatus == 0
+          completion?(success)
+          
+          if !success {
+            NSLog("Script termination status: \(process.terminationStatus)")
+          }
+        }
+      }
+      script.launch()
     }
   }
 }
