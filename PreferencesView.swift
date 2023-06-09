a2295e1abbbacdd4ed01f17debbccec8ccc72dea
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:33 +0200

    Rename Feed to Feeds and try to salvage localizations

--- PreferencesView.swift
+++ PreferencesView.swift
@@ -7,15 +7,20 @@ class PreferencesView: NSView {
   override func awakeFromNib() {
     super.awakeFromNib()
     
+    for item in toolBar.items {
+      item.label = NSLocalizedString(item.itemIdentifier.rawValue, comment: "")
+    }
+    
     if #available(OSX 11.0, *) {
-      let symbolsByIdentifier: [NSToolbarItem.Identifier:String] = [
-        .init("Feed"):      "tray.2",
-        .init("Downloads"): "square.and.arrow.down",
-        .init("Tweaks"):    "gearshape"
+      let symbolsByIdentifier: [String:String] = [
+        "Feeds":     "tray.2",
+        "Downloads": "square.and.arrow.down",
+        "Tweaks":    "gearshape"
       ]
       
       for item in toolBar.items {
-        if let symbolName = symbolsByIdentifier[item.itemIdentifier] {
+        if let symbolName = symbolsByIdentifier[item.itemIdentifier.rawValue] {
+          
           item.image = NSImage(
             systemSymbolName: symbolName,
             accessibilityDescription: nil
