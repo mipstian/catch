d5ddc85d2510f3bfe57933708de58279e0394e9f
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:22 +0200

    Style

--- CTCLoginItems.m
+++ CTCLoginItems.m
@@ -10,7 +10,7 @@
 	// Code totally ripped off from:
 	// http://cocoatutorial.grapewave.com/2010/02/creating-andor-removing-a-login-item/
 	
-	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+	NSString *appPath = NSBundle.mainBundle.bundlePath;
 	CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:appPath];
 	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL, kLSSharedFileListSessionLoginItems, NULL);
 	
@@ -22,7 +22,7 @@
 	if (shouldRegister) {
 		// Add Catch to the login items
 		LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(loginItems,kLSSharedFileListItemLast, NULL, NULL, url, NULL, NULL);
-		if (item){
+		if (item) {
 			CFRelease(item);
 		}
 	} else {
@@ -30,11 +30,11 @@
 		UInt32 seedValue;
 		NSArray *loginItemsArray = (NSArray *)CFBridgingRelease(LSSharedFileListCopySnapshot(loginItems, &seedValue));
         
-		for(NSUInteger i = 0; i < loginItemsArray.count; i++){
+		for(NSUInteger i = 0; i < loginItemsArray.count; i++) {
 			LSSharedFileListItemRef itemRef = (__bridge LSSharedFileListItemRef)[loginItemsArray objectAtIndex:i];
 			// Resolve the item with URL
-			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr) {
-				NSString * urlPath = [(__bridge NSURL*)url path];
+			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*)&url, NULL) == noErr) {
+				NSString *urlPath = [(__bridge NSURL*)url path];
 				if ([urlPath compare:appPath] == NSOrderedSame){
 					// Here I am. Remove me please.
 					LSSharedFileListItemRemove(loginItems,itemRef);
