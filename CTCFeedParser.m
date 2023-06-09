6bbbed2a0155a63f881d7bcd5e122f6e97d22d1e
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:27 +0200

    Use new generic namespaces to extract show names (falling back to the old ones)

--- CTCFeedParser.m
+++ CTCFeedParser.m
@@ -9,7 +9,7 @@
     
     NSError *error = nil;
     
-    // Get file URLs with XPath
+    // Get files with XPath
     NSArray *fileNodes = [feed nodesForXPath:@"//rss/channel/item" error:&error];
     
     if (!fileNodes) {
@@ -17,13 +17,22 @@
         return nil;
     }
     
-    // Extract URLs from NSXMLNodes
+    // Extract files from NSXMLNodes
     NSMutableArray *feedFiles = [NSMutableArray arrayWithCapacity:fileNodes.count];
     
     for(NSXMLNode *fileNode in fileNodes) {
+        // Get the .torrent URL or magnet link
         NSString *url = [[[fileNode nodesForXPath:@"enclosure/@url" error:&error] lastObject] stringValue];
+        
+        // Get the title (includes show name and season/episode numbers)
         NSString *title = [[[fileNode nodesForXPath:@"title" error:&error] lastObject] stringValue];
-        NSString *showName = [[[fileNode nodesForXPath:@"showrss:showname" error:&error] lastObject] stringValue];
+        
+        // Get the show name from the generic "tv:" namespace, fallback to the old "showrss:" namespace
+        NSString *showName = [[[fileNode nodesForXPath:@"tv:show_name" error:&error] lastObject] stringValue];
+        if (!showName) {
+            showName = [[[fileNode nodesForXPath:@"showrss:showname" error:&error] lastObject] stringValue];
+        }
+        
         [feedFiles addObject:@{@"title": title,
                                @"url": url,
                                @"showName": showName ?: NSNull.null}];
