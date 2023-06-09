e38678afca931f117cfff94d98f2b5539e991ed8
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:24 +0200

    Fix cut/copy/paste/select all/undo

---
+++ CTCApplication.h
@@ -0,0 +1,4 @@
+#import <Cocoa/Cocoa.h>
+
+
+@interface CTCApplication : NSApplication @end
---
+++ CTCApplication.m
@@ -0,0 +1,44 @@
+#import "CTCApplication.h"
+
+
+@implementation CTCApplication
+
+/**
+    @brief Reimplement basic cut/copy/paste/undo/select all events
+    @see http://stackoverflow.com/questions/970707/cocoa-keyboard-shortcuts-in-dialog-without-an-edit-menu
+ */
+- (void)sendEvent:(NSEvent *)event {
+    if (event.type == NSKeyDown) {
+        if ((event.modifierFlags & NSDeviceIndependentModifierFlagsMask) == NSCommandKeyMask) {
+            if ([event.charactersIgnoringModifiers isEqualToString:@"x"]) {
+                if ([self sendAction:@selector(cut:) to:nil from:self])
+                    return;
+            }
+            else if ([event.charactersIgnoringModifiers isEqualToString:@"c"]) {
+                if ([self sendAction:@selector(copy:) to:nil from:self])
+                    return;
+            }
+            else if ([event.charactersIgnoringModifiers isEqualToString:@"v"]) {
+                if ([self sendAction:@selector(paste:) to:nil from:self])
+                    return;
+            }
+            else if ([event.charactersIgnoringModifiers isEqualToString:@"z"]) {
+                if ([self sendAction:@selector(undo:) to:nil from:self])
+                    return;
+            }
+            else if ([event.charactersIgnoringModifiers isEqualToString:@"a"]) {
+                if ([self sendAction:@selector(selectAll:) to:nil from:self])
+                    return;
+            }
+        }
+        else if ((event.modifierFlags & NSDeviceIndependentModifierFlagsMask) == (NSCommandKeyMask | NSShiftKeyMask)) {
+            if ([event.charactersIgnoringModifiers isEqualToString:@"Z"]) {
+                if ([self sendAction:@selector(redo:) to:nil from:self])
+                    return;
+            }
+        }
+    }
+    [super sendEvent:event];
+}
+
+@end
