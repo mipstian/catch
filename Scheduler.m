7c86d00a4afccd46c941583da46f9883bb1e9b4b
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:22 +0200

    Removed all that awful threading stuff, wired everything. Time to move the actual code.

--- Scheduler.m
+++ Scheduler.m
@@ -36,53 +36,41 @@
     self.feedCheckerConnection.remoteObjectInterface = [NSXPCInterface interfaceWithProtocol:@protocol(CTCFeedCheck)];
     [self.feedCheckerConnection resume];
 	
-	// run a runloop in another thread
-	[self performSelectorInBackground:@selector(loopRun) withObject:nil];
-    
-    [self callFeedChecker];
-	
-	return self;
-}
-
-- (void)callFeedChecker {
-    CTCFeedChecker *feedChecker = [self.feedCheckerConnection remoteObjectProxy];
-    [feedChecker checkShowRSSFeed:nil
-                downloadingToPath:nil
-               organizingByFolder:NO
-                     skippingURLs:@[]
-                        withReply:^(NSError *error) {
-                        }];
-}
-
-- (void)loopRun {
-	NSLog(@"Scheduler: running RunLoop");
-	
-	@autoreleasepool {
-		NSRunLoop *currentRunLoop = NSRunLoop.currentRunLoop;
-
-		self.repeatingTimer = [NSTimer scheduledTimerWithTimeInterval:FEED_UPDATE_INTERVAL
+	// Create a timer to check periodically
+    self.repeatingTimer = [NSTimer scheduledTimerWithTimeInterval:FEED_UPDATE_INTERVAL
 														   target:self
 														 selector:@selector(tick:)
 														 userInfo:nil
 														  repeats:YES];
-		[currentRunLoop addTimer:self.repeatingTimer forMode:NSDefaultRunLoopMode];
-		[currentRunLoop run];
-	}
+	
+	return self;
+}
+
+- (void)callFeedCheckerWithReplyHandler:(void (^)(NSError *))replyHandler {
+    NSURL *feedURL = [NSURL URLWithString:Preferences.feedURL];
+    NSString *downloadPath = [NSUserDefaults.standardUserDefaults stringForKey:PREFERENCE_KEY_SAVE_PATH];
+    BOOL organizeByFolder = [NSUserDefaults.standardUserDefaults boolForKey:PREFERENCE_KEY_ORGANIZE_TORRENTS];
+    NSArray *previouslyDownloadedURLs = [NSUserDefaults.standardUserDefaults arrayForKey:PREFERENCE_KEY_HISTORY];
+    
+    CTCFeedChecker *feedChecker = [self.feedCheckerConnection remoteObjectProxy];
+    [feedChecker checkShowRSSFeed:feedURL
+                downloadingToPath:downloadPath
+               organizingByFolder:organizeByFolder
+                     skippingURLs:previouslyDownloadedURLs
+                        withReply:replyHandler];
 }
 
 - (void)reportStatus {
-	// Call to this method are performed on the main thread to get them out
-	// in the right order
 	NSLog(@"Scheduler: reporting status: active = %d, running = %d", self.isActive, self.isRunning);
 	
-	// Report status to application controller
+	// Report status to application delegate
 	[[NSApp delegate] schedulerStatusActive:self.isActive running:self.isRunning];
 }
 
 - (BOOL)pauseResume {
 	self.active = !self.isActive;
 	
-	[self performSelectorOnMainThread:@selector(reportStatus) withObject:nil waitUntilDone:NO];
+    [self reportStatus];
 	
 	return self.active;
 }
@@ -90,19 +78,12 @@
 - (void)forceCheck {
 	NSLog(@"Scheduler: forcing check");
     
-    if (!self.repeatingTimer) {
-        NSLog(@"Scheduler: failed to force check, race condition with init");
-        return;
-    }
-    
 	// Set the next timer fire date to be ASAP
 	[self.repeatingTimer setFireDate:NSDate.distantPast];
 }
 
 - (void)tick:(NSTimer*)timer {
 	NSLog(@"Scheduler: tick");
-	
-	BOOL status = NO;
 	
 	if (!self.isActive) {
 		NSLog(@"Scheduler: tick skipped, paused");
@@ -125,23 +106,23 @@
 	
 	self.running = YES;
 	
-	[self performSelectorOnMainThread:@selector(reportStatus) withObject:nil waitUntilDone:NO];
+    [self reportStatus];
 	
-	status = [self.feedChecker checkFeed];
-	
-	self.running = NO;
-	
-	[self performSelectorOnMainThread:@selector(reportStatus) withObject:nil waitUntilDone:NO];
-	
-	[[NSApp delegate] lastUpdateStatus:status time:[NSDate date]];
+    [self callFeedCheckerWithReplyHandler:^(NSError *error){
+        BOOL wasCheckSuccessful = error == nil;
+        self.running = NO;
+        [self reportStatus];
+        [[NSApp delegate] lastUpdateStatus:wasCheckSuccessful
+                                      time:NSDate.date];
+    }];
 }
 			
 - (BOOL)checkTime {
-	NSDate* now = [NSDate date];
-	NSDate* from = (NSDate*) [[NSUserDefaults standardUserDefaults] objectForKey:PREFERENCE_KEY_UPDATE_FROM];
-	NSDate* to = (NSDate*) [[NSUserDefaults standardUserDefaults] objectForKey:PREFERENCE_KEY_UPDATE_TO];
+	NSDate* now = NSDate.date;
+	NSDate* from = (NSDate *)[NSUserDefaults.standardUserDefaults objectForKey:PREFERENCE_KEY_UPDATE_FROM];
+	NSDate* to = (NSDate *)[NSUserDefaults.standardUserDefaults objectForKey:PREFERENCE_KEY_UPDATE_TO];
 	
-	NSCalendar* calendar = [NSCalendar currentCalendar];
+	NSCalendar* calendar = NSCalendar.currentCalendar;
 	
 	// Get minutes and hours from each date
 	NSDateComponents* nowComp = [calendar components:NSHourCalendarUnit|NSMinuteCalendarUnit
@@ -151,20 +132,20 @@
 	NSDateComponents* toComp = [calendar components:NSHourCalendarUnit|NSMinuteCalendarUnit
 												  fromDate:to];
 	
-	if ([fromComp hour] > [toComp hour] ||
-		([fromComp hour] == [toComp hour] && [fromComp minute] > [toComp minute])) {
+	if (fromComp.hour > toComp.hour ||
+		(fromComp.hour == toComp.hour && fromComp.minute > toComp.minute)) {
 		// Time range crosses midnight (e.g. 11 PM to 3 AM)
-		if (([nowComp hour] > [toComp hour] && [nowComp hour] < [fromComp hour]) ||
-			([nowComp hour] == [toComp hour] && [nowComp minute] > [toComp minute]) ||
-			([nowComp hour] == [fromComp hour] && [nowComp minute] < [fromComp minute])) {
+		if ((nowComp.hour > toComp.hour && nowComp.hour < fromComp.hour) ||
+			(nowComp.hour == toComp.hour && nowComp.minute > toComp.minute) ||
+			(nowComp.hour == fromComp.hour && nowComp.minute < fromComp.minute)) {
 			// We are outside of allowed time range
 			return NO;
 		}
 	} else {
 		// Time range doesn't cross midnight (e.g. 4 AM to 5 PM)
-		if (([nowComp hour] > [toComp hour] && [nowComp hour] < [fromComp hour]) ||
-			([nowComp hour] == [toComp hour] && [nowComp minute] > [toComp minute]) ||
-			([nowComp hour] == [fromComp hour] && [nowComp minute] < [fromComp minute])) {
+		if ((nowComp.hour > toComp.hour && nowComp.hour < fromComp.hour) ||
+			(nowComp.hour == toComp.hour && nowComp.minute > toComp.minute) ||
+			(nowComp.hour == fromComp.hour && nowComp.minute < fromComp.minute)) {
 			// We are outside of allowed time range
 			return NO;
 		}
