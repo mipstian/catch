f19706d68810d40860545285f4a37fdf2d34d47d
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:24 +0200

    Move global actions to CTCApplication

--- CTCApplication.m
+++ CTCApplication.m
@@ -1,7 +1,33 @@
 #import "CTCApplication.h"
+#import "CTCDefaults.h"
 
 
 @implementation CTCApplication
+
+- (IBAction)browseService:(id)sender {
+	// Launch the system browser, open the service (ShowRSS)
+	[NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:kCTCDefaultsServiceURL]];
+}
+
+- (IBAction)browseWebsite:(id)sender {
+	// Launch the system browser, open the applications's website
+	[NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:kCTCDefaultsApplicationWebsiteURL]];
+}
+
+- (IBAction)browseHelp:(id)sender {
+	// Launch the system browser, open the applications's on-line help
+	[NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:kCTCDefaultsApplicationHelpURL]];
+}
+
+- (IBAction)browseFeatureRequest:(id)sender {
+	// Launch the system browser, open the applications's feature request page
+	[NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:kCTCDefaultsApplicationFeatureRequestURL]];
+}
+
+- (IBAction)browseBugReport:(id)sender {
+	// Launch the system browser, open the applications's bug report page
+	[NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:kCTCDefaultsApplicationBugReportURL]];
+}
 
 /**
     @brief Reimplement basic cut/copy/paste/undo/select all events
