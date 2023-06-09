1487e249ef31b9cb7abed3a29e7f7601c9532b1b
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:24 +0200

    Move notification center stuff from the menu controller to the app delegate

--- CTCAppDelegate.m
+++ CTCAppDelegate.m
@@ -10,11 +10,19 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	// Set default values for defaults
 	[CTCDefaults setDefaultDefaults];
+    
+	// Enable Notification Center notifications
+    [NSUserNotificationCenter.defaultUserNotificationCenter setDelegate:self];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
 	// Save defaults
 	[CTCDefaults save];
+}
+
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center
+	 shouldPresentNotification:(NSUserNotification *)notification {
+	return YES;
 }
 
 @end
--- CTCMenuController.h
+++ CTCMenuController.h
@@ -1,4 +1,4 @@
 #import <Foundation/Foundation.h>
 
 
-@interface CTCMenuController : NSObject <NSUserNotificationCenterDelegate> @end
+@interface CTCMenuController : NSObject @end
