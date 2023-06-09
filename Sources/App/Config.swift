d151485a3e3f2c69926d8730619416502447770b
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:31 +0200

    Limit history size

--- Sources/App/Config.swift
+++ Sources/App/Config.swift
@@ -1,11 +1,18 @@
 import Foundation
 
 
-/// Static configuration parameters
+/// Static configuration parameters.
 enum Config {
-  /// How often to check feeds
+  /// How often to check feeds.
   static let feedUpdateInterval: TimeInterval = 60 * 10
 
-  /// How much leeway to give to the os for scheduling
+  /// How much leeway to give to the os for scheduling.
   static let feedUpdateIntervalTolerance: TimeInterval = 30
+  
+  /// Maximum number of episodes to keep in the download history.
+  ///
+  /// - SeeAlso: `downloadHistory` in `Defaults`
+  /// - Note: this should be always higher than the number of episodes in a feed,
+  ///         otherwise we'd end up re-downloading episodes over and over.
+  static let historyLimit = 200
 }
