c826e633e6ddfa8d549bf4dccd3eb122c30b3de9
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:29 +0200

    Move feed update interval parameter to Config

--- Sources/Config.swift
+++ Sources/Config.swift
@@ -1,8 +1,11 @@
 import Foundation
 
 
-/// A wrapper for accessing Info.plist values
+/// A wrapper for accessing Info.plist values and configuration parameters
 enum Config {
+  // Check feed every 10 minutes
+  static let feedUpdateInterval: TimeInterval = 60 * 10
+  
   static let appName = infoString(key: "CFBundleDisplayName")
   static let appVersion = infoString(key: "CFBundleShortVersionString")
   static let buildNumber = infoString(key: "CFBundleVersion")
