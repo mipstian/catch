fa146fb1d19a5f5e2ff9bb339cd7a3588b6da16f
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:21 +0200

    tweaks for showrss 3

--- Catch.m
+++ Catch.m
@@ -11,13 +11,13 @@
 
 // Constant, non-localized, non-UI-related strings
 NSString* const APPLICATION_NAME = @"Catch";
-NSString* const APPLICATION_VERSION = @"1.4";
-NSString* const APPLICATION_WEBSITE_URL = @"http://code.google.com/p/catch/";
-NSString* const APPLICATION_BUG_REPORT_URL = @"http://code.google.com/p/catch/issues/entry?template=Defect%20report%20from%20user";
-NSString* const APPLICATION_FEATURE_REQUEST_URL = @"http://code.google.com/p/catch/issues/entry?template=Feature%20request";
-NSString* const APPLICATION_HELP_URL = @"http://code.google.com/p/catch/wiki/Help";
-NSString* const SERVICE_URL = @"http://showrss.karmorra.info/";
-NSString* const SERVICE_FEED_URL_PREFIX = @"http://showrss.karmorra.info/rss.php?";
+NSString* const APPLICATION_VERSION = @"1.5";
+NSString* const APPLICATION_WEBSITE_URL = @"http://github.com/showrss/catch";
+NSString* const APPLICATION_BUG_REPORT_URL = @"https://github.com/showrss/catch/issues/new";
+NSString* const APPLICATION_FEATURE_REQUEST_URL = @"https://github.com/showrss/catch/issues/new";
+NSString* const APPLICATION_HELP_URL = @"https://github.com/showrss/catch/wiki/Help";
+NSString* const SERVICE_URL = @"http://showrss.info/";
+NSString* const SERVICE_FEED_URL_PREFIX = @"http://showrss.info/rss.php?";
 
 
 @implementation Catch
