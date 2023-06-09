ee000b23c61c36b555388fde8d6a94418c4a717e
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:33 +0200

    Use SF Symbols for recents cell button, make it a bit larger

--- Sources/App/RecentsCellView.swift
+++ Sources/App/RecentsCellView.swift
@@ -4,4 +4,15 @@ import AppKit
 class RecentsCellView: NSTableCellView {
   @IBOutlet weak var downloadDateTextField: NSTextField!
   @IBOutlet weak var downloadAgainButton: NSButton!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    if #available(OSX 11.0, *) {
+      downloadAgainButton.image = NSImage(
+        systemSymbolName: "arrow.clockwise.circle.fill",
+        accessibilityDescription: nil
+      )?.withSymbolConfiguration(.init(scale: .large))
+    }
+  }
 }
