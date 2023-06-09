f9c3f25369aba70bedf006541633813550e70b95
Kaylee <kaylee@kaylees.site>
2023-06-09 19:11:34 +0200

    Localize a couple things

--- Sources/App/PreferencesView.swift
+++ Sources/App/PreferencesView.swift
@@ -2,10 +2,16 @@ import AppKit
 
 
 class PreferencesView: NSView {
+  @IBOutlet weak var downloadScriptCheckbox: NSButton!
+  @IBOutlet weak var importFromOPMLButton: NSButton!
+  
   @IBOutlet weak var toolBar: NSToolbar!
   
   override func awakeFromNib() {
     super.awakeFromNib()
+    
+    importFromOPMLButton.title = NSLocalizedString("Import From OPML File…", comment: "")
+    downloadScriptCheckbox.title = NSLocalizedString("Download using script:", comment: "")
     
     for item in toolBar.items {
       item.label = NSLocalizedString(item.itemIdentifier.rawValue, comment: "")
